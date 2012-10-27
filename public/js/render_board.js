034360c9cbc434f4f1c5265243d893ecb313cd4e
Drin <DrinMontana@gmail.com>
2012-10-27 02:24:09 -0700

    In the process of building spiffy new render_board

--- public/js/render_board.js
+++ public/js/render_board.js
@@ -1,25 +1,39 @@
 define(['board_constants'], function(board_consts) {
-   return function(board_props) {
-      var board = '';
+   return function(board_map) {
+      var boardHTML = '';
 
-      for (var dimX = 0; dimX < board_props.rows; dimX++) {
+      console.log(board_map);
+
+      for (var dimX = 0; dimX < board_map.length; dimX++) {
          var xPos = dimX * board_consts.TILE_DIM;
 
-         for (var dimY = 0; dimY < board_props.cols; dimY++) {
-            var yPos = dimY * board_consts.TILE_DIM;
-            var tileClass = board_consts.TILE_CLASS + ' ' +
-                            board_props.map[dimX][dimY];
+         console.log(dimX);
 
-            var tileComponent = '<div class="' + tileClass + '" ' +
+         for (var dimY = 0; dimY < board_map[dimX].length; dimY++) {
+            var yPos = dimY * board_consts.TILE_DIM;
+
+            var tileObj = board_map[dimX][dimY];
+
+            var unitObj = tileObj.unit.render();
+            var terrainObj = tileObj.terrain.render();
+
+            var tileClass = board_consts.TILE_CLASS;
+
+            var tileComponent = '<div> ' +
+                                'class="' + tileClass + '" ' +
                                 'data-x="' + dimX + '" ' +
                                 'data-y="' + dimY + '" ' +
-                                'style="position: absolute; top: ' +
-                                xPos + 'px; left: ' + yPos + 'px;"></div>';
+                                'style="position: absolute; ' +
+                                       'top: ' + xPos + 'px; ' +
+                                       'left: ' + yPos + 'px;">' +
+                                unitObj.innerHTML +
+                                terrainObj.innerHTML +
+                                '</div>';
 
-            board += tileComponent;
+            boardHTML += tileComponent;
          }
       }
 
-      document.getElementById(board_consts.BOARD_COMPONENT).innerHTML = board;
+      document.getElementById(board_consts.BOARD_COMPONENT).innerHTML = boardHTML;
    }
 });
