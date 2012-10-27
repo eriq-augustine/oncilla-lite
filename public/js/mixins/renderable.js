7be0ecf09dbd06816c720ac4aefaf4f867b41860
Matt Tognetti <mjtognetti@gmail.com>
2012-10-27 03:49:51 -0700

    added shitty health indicators

--- public/js/mixins/renderable.js
+++ public/js/mixins/renderable.js
@@ -5,6 +5,11 @@ define(function() {
             var className = this.className || 'default',
                 el = document.createElement('div');
             el.className = options.baseClass + ' ' + this.className;
+
+            if (this.health) {
+               el.innerHTML = this.health;
+            }
+
             return el;
          }
       }
